756ea0c5aa9b635b697ee356bf9793eeb9e6acbf
Cheikh Gaye Gadji <cheikhgayegadji@gmail.com>
2025-08-02 00:48:43 +0000

    feat: add OddsTrackerPage with OddsHero and OddsTable components
    Co-authored-by: Genie <genie@cosine.sh>

---
+++ next-app/app/odds-tracker/page.tsx
@@ -0,0 +1,11 @@
+import { OddsHero } from "../../components/OddsHero";
+import { OddsTable } from "../../components/OddsTable";
+
+export default function OddsTrackerPage() {
+  return (
+    <main>
+      <OddsHero />
+      <OddsTable />
+    </main>
+  );
+}
